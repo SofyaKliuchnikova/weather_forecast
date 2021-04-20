e2088ec96d2e53624ab2f77c9e81f97d66c3a15c
SofyaKliuchnikova <78865206+SofyaKliuchnikova@users.noreply.github.com>
2021-04-20 14:34:07 +0300

    Add files via upload

--- entity/DailyForecastModelForDB.java
+++ entity/DailyForecastModelForDB.java
@@ -1,7 +1,6 @@
 package Homework6.entity;
 
 public class DailyForecastModelForDB {
-    //(id integer primary key autoincrement, city text, localdate text, tempMin real, tempMax real, textDay text, textNight text);
     private String city;
     private String localDate;
     private double tempMin;
@@ -12,60 +11,57 @@ public class DailyForecastModelForDB {
     public DailyForecastModelForDB() {
     }
 
-    public DailyForecastModelForDB(String city, String localDate, double tempMin, double tempMax, String textDay, String textNight) {
-        this.city = city;
-        this.localDate = localDate;
-        this.tempMin = tempMin;
-        this.tempMax = tempMax;
-        this.textDay = textDay;
-        this.textNight = textNight;
-    }
-
     public String getCity() {
         return city;
     }
 
-    public void setCity(String city) {
+    public DailyForecastModelForDB setCity(String city) {
         this.city = city;
+        return this;
     }
 
     public String getLocalDate() {
         return localDate;
     }
 
-    public void setLocalDate(String localDate) {
+    public DailyForecastModelForDB setLocalDate(String localDate) {
         this.localDate = localDate;
+        return this;
     }
 
     public double getTempMin() {
         return tempMin;
     }
 
-    public void setTempMin(double tempMin) {
+    public DailyForecastModelForDB setTempMin(double tempMin) {
         this.tempMin = tempMin;
+        return this;
     }
 
     public double getTempMax() {
         return tempMax;
     }
 
-    public void setTempMax(double tempMax) {
+    public DailyForecastModelForDB setTempMax(double tempMax) {
         this.tempMax = tempMax;
+        return this;
     }
 
     public String getTextDay() {
         return textDay;
     }
 
-    public void setTextDay(String textDay) {
+    public DailyForecastModelForDB setTextDay(String textDay) {
         this.textDay = textDay;
+        return this;
     }
 
     public String getTextNight() {
         return textNight;
     }
 
-    public void setTextNight(String textNight) {
+    public DailyForecastModelForDB setTextNight(String textNight) {
         this.textNight = textNight;
+        return this;
     }
 }
